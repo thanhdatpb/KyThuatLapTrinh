c97390391ed912c819b7da6ba620e262943ded80
hoanganh <21t1020003@husc.edu.vn>
2022-03-08 18:25:14 +0700

    cch

--- TEAM02/NHAnh/main.cpp
+++ TEAM02/NHAnh/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
   printf("WELCOME C/C++\n");
 
   LOG_D("Logger error\n");
+  LOG_DT("Logger error\n");
   LOG_W("Logger warning\n");
   LOG_I("Logger info\n");
   LOG_D("Logger debug\n");
