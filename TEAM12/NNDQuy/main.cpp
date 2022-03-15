ebcf392a77017ff943365a7beefe5c6e8be7faa2
Ngô Nguyễn Đức Quý <email>
2022-03-15 15:27:06 +0700

    quy

--- TEAM12/NNDQuy/main.cpp
+++ TEAM12/NNDQuy/main.cpp
@@ -1,6 +1,6 @@
 #include "stdio.h"
 #include "../../_src/Log.h"
-
+using namespace std;
 int main(int argc, char const *argv[])
 {  
   printf("WELCOME C/C++\n");
@@ -13,5 +13,6 @@ int main(int argc, char const *argv[])
   LOG_W("LOGGER WARNING\n");
   LOG_I("LOGGER INFO\n");
   LOG_D("LOGGER DEBUG\n");
+  printf("\n*******************\n");
   return 0;
 }
