b0a016d1e8458ff8942d767bb89105320c7aa9fe
Ngô Nguyễn Đức Quý <email>
2022-03-15 16:25:31 +0700

    quy

--- TEAM12/NNDQuy/main.cpp
+++ TEAM12/NNDQuy/main.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 int main()
 {
+  printf("WELCOME C/C++\n");
+  LOG_ET("LOGGER ERROR\n");
+  LOG_WT("LOGGER WARNING\n");
+  LOG_IT("LOGGER INFO\n");
+  LOG_DT("LOGGER DEBUG\n");
+
+  LOG_E("LOGGER ERROR\n");
+  LOG_W("LOGGER WARNING\n");
+  LOG_I("LOGGER INFO\n");
+  LOG_D("LOGGER DEBUG\n");
+  printf("/n*******************/n");
   char ten[]="Ngo Nguyen Duc Quy";
   char lop[]="ktlt - n14";
   int thang=3;
