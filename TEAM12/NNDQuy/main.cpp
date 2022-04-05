bb104af3e7179fba0517deb7f43195125eeb2c65
Ngô Nguyễn Đức Quý <email>
2022-04-05 15:29:22 +0700

    quy

--- TEAM12/NNDQuy/main.cpp
+++ TEAM12/NNDQuy/main.cpp
@@ -10,10 +10,10 @@ int main()
   LOG_IT("Msv: 21T1020155\n");
   LOG_DT("-----\n");
   string s;
-  scanf("%s",&s);
   int ans=0;
   string t="";
-  LOG_DT("Nhap chuoi: %s\n",&s);
+  LOG_DT("Nhap chuoi: \n");
+  getline(cin,s);
   for(char ch : s)
   {
     if(ch>'0'&& ch <='9')
