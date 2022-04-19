31021eaf2e2e4dfc744453faa8848dfb3e697939
Lê Quang Khải <21t1020204@husc.edu.vn>
2022-04-19 15:25:28 +0700

    LQK

--- TEAM01/LQKHAI/BAIKIEMTRA.cpp
+++ TEAM01/LQKHAI/BAIKIEMTRA.cpp
@@ -1,35 +1,35 @@
-# include  < bits / stdc ++. h >
-# bao gồm  " ../../_src/Log.h "
-sử dụng  không gian tên  std ;
-# định  nghĩa  dài  dài
-int  Eratos ( int n) {
-    for ( int i = 2 ; i <= sqrt (n); i ++) {
-        if (n% i == 0 ) trả về  0 ;
+#include <bits/stdc++.h>
+#include "../../_src/Log.h"
+using namespace std;
+#define ll long long
+int Eratos(int n){
+    for( int i = 2; i <= sqrt(n); i++ ){
+        if(n % i == 0) return 0;
     }
-    trả về n> 1 ;
+    return n > 1;
 }
-void  Res (string & s) {
-    for ( int i = 0 ; i <s. size (); i ++) {
-        ll ans = 0 ;
-        while ( isdigit (s [i])) {
-            ans = ans * 10 + s [i] - ' 0 ' ;
-            i ++;
+void Res( string &s){
+    for( int i = 0; i < s.size(); i++ ){
+        ll ans = 0;
+        while(isdigit(s[i])){
+            ans = ans * 10 + s[i] - '0';
+            i++;
         }
-        if ( Eratos (ans)) cout << ans << "  " ;
+        if(Eratos(ans)) cout << ans << " ";
     }
 }
-void  Đầu ra () {
-    dây;
-    LOG_ET ( " Bai: Loc so nguyen to trong xau \ n " );
-    LOG_WT ( " Ten: Le Quang Khai \ n " );
-    LOG_IT ( " Mã sv: 21T1020204 \ n " );
-    LOG_DT ( " _____ \ n " );
-    LOG_ET ( " Nhap chuoi: " ); getline (cin, s);
-    LOG_WT ( " So nguyen to: " ); Res (s);
+void Output(){
+    string s; 
+    LOG_ET( "Bai : Loc so nguyen to trong xau\n");
+    LOG_WT("Ten: Le Quang Khai\n");
+    LOG_IT("Ma sv: 21T1020204\n");
+    LOG_DT("_____\n");
+    LOG_ET( "Nhap chuoi: " ); getline( cin, s );
+    LOG_WT( "So nguyen to: " ); Res(s);
 }
-int  main () {
-    ios_base :: sync_with_stdio ( 0 ); cin. hòa ( 0 ); cout. hòa ( 0 );
-    Đầu ra ();
-    trả về  0 ;
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    Output();
+    return 0;
 }
 
